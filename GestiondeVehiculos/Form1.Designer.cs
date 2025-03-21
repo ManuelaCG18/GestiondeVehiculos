7096fcd64ea1186510c2790e24aa663e78ed8b90
ManuelaCG18 <manuela.castano585@pascualbravo.edu.co>
2025-03-20 19:39:26 -0500

    Agregar archivos de proyecto.

---
+++ GestiondeVehiculos/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GestiondeVehiculos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.cbTipoVehiculo = new System.Windows.Forms.ComboBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtPrecioBase = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lvVehiculos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(83, 136);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(154, 20);
+            this.txtModelo.TabIndex = 0;
+            // 
+            // cbTipoVehiculo
+            // 
+            this.cbTipoVehiculo.FormattingEnabled = true;
+            this.cbTipoVehiculo.Items.AddRange(new object[] {
+            "Automovil",
+            "Motocicleta"});
+            this.cbTipoVehiculo.Location = new System.Drawing.Point(83, 83);
+            this.cbTipoVehiculo.Name = "cbTipoVehiculo";
+            this.cbTipoVehiculo.Size = new System.Drawing.Size(152, 21);
+            this.cbTipoVehiculo.TabIndex = 1;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(83, 192);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(154, 20);
+            this.txtMarca.TabIndex = 2;
+            // 
+            // txtPrecioBase
+            // 
+            this.txtPrecioBase.Location = new System.Drawing.Point(83, 247);
+            this.txtPrecioBase.Name = "txtPrecioBase";
+            this.txtPrecioBase.Size = new System.Drawing.Size(154, 20);
+            this.txtPrecioBase.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo de vehiculo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Modelo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Marca:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Precio base:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(88, 342);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(125, 31);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lvVehiculos
+            // 
+            this.lvVehiculos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvVehiculos.HideSelection = false;
+            this.lvVehiculos.Location = new System.Drawing.Point(344, 71);
+            this.lvVehiculos.Name = "lvVehiculos";
+            this.lvVehiculos.Size = new System.Drawing.Size(567, 302);
+            this.lvVehiculos.TabIndex = 10;
+            this.lvVehiculos.UseCompatibleStateImageBehavior = false;
+            this.lvVehiculos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Precio Final";
+            this.columnHeader1.Width = 74;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo Vehiculo";
+            this.columnHeader2.Width = 91;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Modelo";
+            this.columnHeader3.Width = 75;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Marca";
+            this.columnHeader4.Width = 70;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Precio Base";
+            this.columnHeader5.Width = 83;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Cargo Extra";
+            this.columnHeader6.Width = 81;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Impuesto";
+            this.columnHeader7.Width = 76;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 450);
+            this.Controls.Add(this.lvVehiculos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecioBase);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.cbTipoVehiculo);
+            this.Controls.Add(this.txtModelo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.ComboBox cbTipoVehiculo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtPrecioBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView lvVehiculos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ GestiondeVehiculos/Vehiculos.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestiondeVehiculos
+{
+    public abstract class Vehiculos
+    {
+        public string Modelo {  get; private set; }
+        public string Marca { get; private set; }
+        public double PrecioBase { get; private set; }
+
+        protected Vehiculos(string modelo, string marca, double precioBase) 
+        { 
+            if (string.IsNullOrWhiteSpace(modelo) || string.IsNullOrWhiteSpace(marca))
+            {
+
+                throw new ArgumentException("El modelo y la marca no pueden estar vacios, son obligatorios :(");
+            }
+            if (precioBase < 0)
+            {
+                throw new ArgumentException("El precio es obligatorio, y debe ser mayor a cero");
+            }
+
+            Modelo = modelo;
+            Marca = marca;
+            PrecioBase = precioBase;
+        }
+
+        public abstract double CalcularPrecioFinal();
+        public abstract double CalcularCargoExtra();
+        public abstract double CalcularImpuesto();
+    }
+
+    public class Automovil : Vehiculos
+    {
+        public Automovil(string modelo, string marca, double precioBase) : base(modelo, marca, precioBase) { }
+
+        public override double CalcularPrecioFinal()
+        {
+            return PrecioBase + CalcularImpuesto();
+        }
+
+        public override double CalcularCargoExtra()
+        {
+            return 0;
+        }
+
+        public override double CalcularImpuesto()
+        {
+            return PrecioBase > 50000000 ? PrecioBase * 0.10 : 0;
+        }
+
+    }
+
+    public class Motocicleta : Vehiculos
+    {
+        public Motocicleta(string modelo, string marca, double precioBase) : base(modelo, marca, precioBase) { }
+
+        public override double CalcularPrecioFinal()
+        {
+            return PrecioBase + CalcularCargoExtra();
+        }
+
+        public override double CalcularCargoExtra()
+        {
+            return PrecioBase > 15000000 ? 500000 : 0;
+        }
+
+        public override double CalcularImpuesto()
+        {
+            return 0;
+        }
+    }
+
+    public class VehiculosFactory
+    {
+        public static Vehiculos CrearVehiculo(string tipo, string modelo, string marca, double precioBase)
+        {
+
+        }
+    }
+    
+    
+}
